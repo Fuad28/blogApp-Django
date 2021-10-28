541070f5f3632f5ce505505447e973b7a9224264
Adio Fuad Abiodun <pro@Fuads-MacBook-Pro.local>
2021-10-28 17:41:34 +0100

    added some deployment config

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # blogApp-Django
 This blog app has very exciting features. It was my stepping stone in learning Django. 
 Check out the full playlist here: https://www.youtube.com/playlist?list=PL-osiE80TeTtoQCKZ03TU5fNfx2UY6U4p
+
+Deployed version : fuaddjangoblogapp.herokuapp.com
--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-gf8pjpu#4=t92yvqe7-osfyjncs^h$o*84zsc8qyp1xzjf731+"
-
+SECRET_KEY= os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://fuaddjangoblogapp.herokuapp.com", 
+'127.0.0.1'
+]
 
 
 # Application definition
@@ -135,11 +137,25 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 EMAIL_BACKEND= 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST= 'smtp.mail.yahoo.com'
 EMAIL_PORT= 587
-EMAIL_HOST_USER= 'fuadadio75@yahoo.com'
-EMAIL_HOST_PASSWORD= 'mzutyuqujitlzmzs'
+EMAIL_HOST_USER= os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD= os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS= True
 EMAIL_USE_SSL= False
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+
+AWS_ACCESS_KEY_ID=  os.environ.get('AWS_ACCESS_KEY_ID') 
+AWS_SECRET_ACCESS_KEY= os.environ.get('AWS_SECRET_ACCESS_KEY') 
+AWS_STORAGE_BUCKET_NAME= os.environ.get('AWS_STORAGE_BUCKET_NAME') 
+
+AWS_S3_FILE_OVERWRITE= False
+AWS_DEFAULT_ACL= None
+DEFAULT_FILE_STORAGE= 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_S3_ADDRESSING_STYLE = 'virtual'
+AWS_S3_REGION_NAME = "us-east-2"
+AWS_S3_SIGNATURE_VERSION = "s3v4"
+
+django_heroku.settings(locals())
 
 
 # Default primary key field type
